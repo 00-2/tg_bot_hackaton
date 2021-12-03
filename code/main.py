324cbf15667b69e67eeb30108b80e07a6215ce33
sergey.dmitriev <info1981@yandex.ru>
2021-12-03 17:28:38 +0300

    new code

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import telebot
 import os
 import logging
+from telebot import types
 
 simp_path = 'credentials/bot_token'
 abs_path = os.path.abspath(simp_path)
@@ -16,9 +17,42 @@ logging.basicConfig(filename='server.log', level=logging.DEBUG, format=FORMAT)
 logging.info('bot start')
 bot = telebot.TeleBot(TOKEN)
 
+name = ''
+surname = ''
+
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message: telebot.types.Message):
-    bot.reply_to(message, f"Welcome, {message.chat.username}")
+def start(message: telebot.types.Message):
+    if message.text == '/start':
+        bot.send_message(message.from_user.id, "Напишите ваше имя:")
+        bot.register_next_step_handler(message, get_name)  # следующий шаг – функция get_name
+
+
+# @bot.message_handler(content_types=['text'])
+def get_name(message): #получаем фамилию и имя
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Напишите вашу Фамилию:')
+    bot.register_next_step_handler(message, get_surname)
+
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message, f"Имя, {name}, фамилия {surname}")   # тест
+    bot.send_message(message.from_user.id, 'Выберите подразделение в котором работаете:')
+    bot.register_next_step_handler(message, get_unit)
+
+
+def get_unit(message: types.Message):
+    markup_inline = types.InlineKeyboardMarkup()
+    button_1 = types.InlineKeyboardButton(text='Подразделение 1', callback_data=1)
+    button_2 = types.InlineKeyboardButton(text='Подразделение 2', callback_data=2)
+    button_3 = types.InlineKeyboardButton(text='Подразделение 3', callback_data=3)
+    markup_inline.add(button_1)
+    markup_inline.add(button_2)
+    markup_inline.add(button_3)
+    # bot.send_message(message.chat.id, 'Сделайте выбор', reply_markup=markup_inline)
+
 
 bot.polling(none_stop=True)
