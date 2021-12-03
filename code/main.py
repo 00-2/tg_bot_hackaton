2cee195137c19d6479fa8bdecfe0e71f88b868fe
sergey.dmitriev <info1981@yandex.ru>
2021-12-03 18:24:01 +0300

    buttons made

--- code/main.py
+++ code/main.py
@@ -34,16 +34,17 @@ def get_name(message): #получаем фамилию и имя
     name = message.text
     bot.send_message(message.from_user.id, 'Напишите вашу Фамилию:')
     bot.register_next_step_handler(message, get_surname)
+    bot.register_next_step_handler(message, get_unit)
 
 
 def get_surname(message):
     global surname
     surname = message.text
-    bot.send_message(message.from_user.id, f"Имя, {name}, фамилия {surname}")   # тест
-    bot.register_next_step_handler(message, get_unit)
+
 
 def get_unit(message: types.Message):
-    bot.send_message(message.from_user.id, 'Выберите подразделение в котором работаете:')
+# def get_unit(message):
+
     markup_inline = types.InlineKeyboardMarkup()
     button_1 = types.InlineKeyboardButton(text='Подразделение 1', callback_data=1)
     button_2 = types.InlineKeyboardButton(text='Подразделение 2', callback_data=2)
@@ -51,7 +52,9 @@ def get_unit(message: types.Message):
     markup_inline.add(button_1)
     markup_inline.add(button_2)
     markup_inline.add(button_3)
-    bot.send_message(message.chat.id, 'Сделайте выбор', reply_markup=markup_inline)
+    bot.send_message(message.from_user.id, f"Имя, {name}, фамилия {surname}")   # тест
+    bot.send_message(message.chat.id, 'Выберите подразделение в котором работаете:', reply_markup=markup_inline)
+
 
 
 bot.polling(none_stop=True)
